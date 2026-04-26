0d56ffc3b0484fd4be1b776c47d80816ec4b7ad5
Tim Heitmann <Tim-h@Tim-Laptop.zarm.uni-bremen.de>
2026-04-26 11:06:17 +0200

    h

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def print_results(players):
     # Printe alle spieler mit deren Punktzahl (3 Punkte pro win, Unentschieden 1 Punkt), sortiert und verkünde die Gewinner (die mit höhster Punktzahl)
     players.sort(key=lambda x: (x["Matchpoints"], x["Gamepoints"]), reverse=True)
 
-    for i in range(len(players)):
-        print(i)
+    for i in players:
+        print(f"{i['name']} | Punkte: {i['punkte']} | Siege: {i['siege']}")
 
 print_results([{"name": 'strig', "Matchpoints": 6, "Gamepoints": 12}, {"name": 'strin', "Matchpoints": 4, "Gamepoints": 9}])
